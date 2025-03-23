1d8e749b7dc29b5c04aee87d375d45828a7fef4c
da-br <brandendj@gmail.com>
2025-03-23 21:20:48 +0100

    home manager update

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, lib,  ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "daniel.brandenburg";
   home.homeDirectory = "/home/daniel.brandenburg";
-  
+
   nixpkgs.config.allowUnfree = true;
 
   home.stateVersion = "24.11"; # Please read the comment before changing.
@@ -19,42 +19,28 @@
     zoxide
     eza
     lazygit
-    
+
     # Programming languages and tools
     go
-    
+    zig
+
     # Applications
     jetbrains-toolbox
     jetbrains.rider
     wezterm
     docker
+
+    # Fonts
     fira-code
-    
   ];
-  
+
   fonts.fontconfig.enable = true;
-
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
 
   # Let Home Manager install and manage itself.
   programs = {
     # Let Home Manager install and manage itself.
     home-manager.enable = true;
-    
+
     # Brave configuration for extensions
     brave = {
       enable = true;
@@ -77,7 +63,7 @@
     zsh = {
       enable = true;
       # No config here as it's managed by stow
-      
+
       # Oh My ZSH installation only
       oh-my-zsh = {
         enable = true;
@@ -85,15 +71,13 @@
         theme = "robbyrussell";
       };
     };
-    
-
-
 
     # Git - just install the package
     git = {
-    	enable = true;
-    	userName = "Daniel Brandenburg";
-    	userEmail = "brandendj@gmail.com";
+      enable = true;
+      userName = "Daniel Brandenburg";
+      userEmail = "brandendj@gmail.com";
+      core.autocrlf = false;
     };
 
     # FZF installation only
@@ -109,23 +93,6 @@
     ".config/wezterm".source = "${config.home.homeDirectory}/.dotfiles/.wezterm.lua";
     ".ideavimrc".source = "${config.home.homeDirectory}/.dotfiles/.ideavimrc";
   };
-  
-    # Set default shell to Zsh
-  home.sessionVariables.SHELL = "${pkgs.zsh}/bin/zsh";
-  home.shellAliases = {
-    sh = "zsh";
-  };
-  
-  home.activation.setShell = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    chsh -s ${pkgs.zsh}/bin/zsh ${config.home.username}
-  '';
-  
-    # Set default terminal to WezTerm
-  xdg.mimeApps.defaultApplications = {
-    "x-scheme-handler/terminal" = "org.wezfurlong.wezterm.desktop";
-  };
-  
-
 
   xdg.mimeApps = {
     enable = true;
@@ -133,6 +100,35 @@
       "text/html" = "brave-browser.desktop";
       "x-scheme-handler/http" = "brave-browser.desktop";
       "x-scheme-handler/https" = "brave-browser.desktop";
+      "x-scheme-handler/terminal" = "org.wezfurlong.wezterm.desktop";
+    };
+  };
+
+  services.kanshi = {
+    enabled = true;
+    profiles = {
+      docked = {
+        outputs = [
+          {
+            criteria = "DP-2";
+            status = "enable";
+            position = "0,0";
+          }
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+        ];
+      };
+      undocked = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            status = "enable";
+            position = "0,0";
+          }
+        ];
+      };
     };
   };
 }
